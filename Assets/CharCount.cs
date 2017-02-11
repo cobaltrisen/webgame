970aed39ca610ef80ef5edb5444e52d847c4f6f9
CobaltRisen <cobaltrisen@gmail.com>
2017-02-10 20:24:54 -0500

    added colors, names, chat, and fixed bugs.

---
+++ Assets/CharCount.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CharCount : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Text>().text = (GetComponentInParent<InputField>().characterLimit - GetComponentInParent<InputField>().text.Length).ToString();
+
+    }
+}
---
+++ Assets/Networking.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Networking : MonoBehaviour {
+    GameObject cnv;
+    GameObject gcnv;
+	// Use this for initialization
+	void Start () {
+        cnv = GameObject.Find("Canvas");
+        gcnv = GameObject.Find("GameCanvas");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.FindGameObjectsWithTag("Player").Length == 0) {
+            cnv.SetActive(true);
+            gcnv.SetActive(false);
+            GetComponent<NetworkManagerHUD>().enabled = !cnv.GetComponentInChildren<InputField>().isFocused;
+        } else {
+            cnv.SetActive(false);
+            gcnv.SetActive(true);
+            GetComponent<NetworkManagerHUD>().enabled = false;
+        }
+	}
+    public void ExitGame() {
+        print("RIP");
+        GetComponent<NetworkManager>().StopHost();
+    }
+}
